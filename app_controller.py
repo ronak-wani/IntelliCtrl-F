e7f2eb573a0b573cc5aa785b825c0e0f8d5ee74f
Ronak Wani <ronak_wani@outlook.com>
2024-01-12 15:03:22 -0500

    Basic Flask Setup

---
+++ app_controller.py
@@ -0,0 +1,8 @@
+import base64
+import io
+from flask import render_template, redirect, request, app, flash, send_file
+from io import BytesIO
+
+class appController():
+    def __init__(self, app):
+        pass
