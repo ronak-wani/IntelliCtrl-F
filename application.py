848628cb11f082312deb57482a0c8e3564bb6e06
Ronak Wani <ronak_wani@outlook.com>
2024-01-12 21:20:20 -0500

    text extract from pdf, txt, docx

--- application.py
+++ application.py
@@ -9,43 +9,23 @@ from flask import render_template, redirect, request, app, flash, send_file
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    # def register(self):
-    #     if request.method == "POST":
-    #         if request.form.get("usreg"):
-    #             name = request.form.get("fname")
-    #             usname = request.form.get("usname")
-    #             usemail = request.form.get("usemail")
-    #             usnum = request.form.get("usphnum")
-    #             uspwd = request.form.get("uspwd")
-    #             return redirect("/")
-    #
-    # def showNewProject(self):
-    #     return render_template("new-project.html")
-    #
-    # def userCode(self):
-    #     if request.method == "GET":
-    #         code = request.args.get("code")
-    #         language = request.args.get("language")
-    #         print(code, language)
-    #
-    # def pdf_uploader(self, file):
-    #         filename = secure_filename(file.filename)
-    #         filepath = os.path.join(tempfile.gettempdir(), filename)
-    #         file.save(filepath)
-    #         with open(filepath, "rb") as f:
-    #             reader = PyPDF2.PdfReader(f)
-    #             page = reader.pages[0]
-    #             text = page.extract_text()
-    #         return render_template("new-project.html", text=text)
-    # def txt_uploader(self, file):
-    #         filename = secure_filename(file.filename)
-    #         filepath = os.path.join(tempfile.gettempdir(), filename)
-    #         file.save(filepath)
-    #         with open(filepath, "rb") as f:
-    #             text = f.read()
-    #         return render_template("new-project.html", text=text)
+    def showNewProject(self):
+        return render_template("new-project.html")
+
+    def userCode(self):
+        if request.method == "GET":
+            code = request.args.get("code")
+            language = request.args.get("language")
+            print(code, language)
+    def txt_uploader(self, file):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(tempfile.gettempdir(), filename)
+            file.save(filepath)
+            with open(filepath, "rb") as f:
+                text = f.read()
+            return render_template("new-project.html", text=text)
     #
     # def docx_uploader(self, file):
     #         filename = secure_filename(file.filename)
@@ -56,6 +36,19 @@ def index():
     #         return render_template("new-project.html", text=text)
 
     return render_template("index.html")
-
+@app.route("/pdf_uploader", methods=["GET", "POST"])
+def pdf_uploader():
+    if request.method == 'POST':
+        file = request.files['file']
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(tempfile.gettempdir(), filename)
+    file.save(filepath)
+    with open(filepath, "rb") as f:
+        reader = PyPDF2.PdfReader(f)
+        for i in range(len(reader.pages)):
+            page = reader.pages[i]
+            text = page.extract_text()
+            print(text)
+    return redirect("/")
 if __name__ == "__main__":
     app.run()
