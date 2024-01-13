2a76ef6f7a3a16959b0746e620e7e6140ab1e34c
Ronak Wani <ronak_wani@outlook.com>
2024-01-12 22:59:03 -0500

    Incorporated speech recognition and tts

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import io
 import json
 from docx.api import Document
 import pytesseract
-import os
+import os, pyttsx3, speech_recognition
 import PIL
 from pdfminer.high_level import extract_pages, extract_text
 from PIL.Image import Image
@@ -78,19 +78,12 @@ def pdf_recognition(filepath):
             print(text)
     return redirect("/")
 def txt_recognition(filepath):
-    #if request.method == 'POST':
-        #file = request.files['file']
-    #filename = secure_filename(file.filename)
-    #filepath = os.path.join(tempfile.gettempdir(), filename)
-    #file.save(filepath)
     with open(filepath, "rb") as f:
         text = f.read()
         print(text)
     return redirect("/")
 
 def docx_recognition(filepath):
-    #if request.method == 'POST':
-        #file = request.files['file']
     document = Document(filepath)
     all_text = ""
     for p in document.paragraphs:
@@ -102,6 +95,35 @@ def image_recognition(image: Image):
     myconfig = r"--psm 6 --oem 3"
     text = pytesseract.image_to_string(PIL.Image.open(image), config=myconfig)
     print(text)
+
+def text_to_speech(self):
+    if request.method == "GET":
+        test = request.args.get("tts")
+        print(test)
+        text_speech = pyttsx3.init()
+        text_speech.say(f"{test}")
+        text_speech.runAndWait()
+        return None
+
+def speech_recognition(self):
+    if request.method == "GET":
+        recognizer = speech_recognition.Recognizer()
+             while True:
+                 try:
+                     with speech_recognition.Microphone() as mic:
+                         recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+                         audio = recognizer.listen(mic)
+                         text = recognizer.recognize_google(audio)
+                         text = text.lower()
+                         print(f"Recognized {text}")
+                         return json.dumps(text)
+                 except speech_recognition.UnknownValueError:
+                    # Reset the recognizer
+                    recognizer = speech_recognition.Recognizer()
+                    # Continue listening
+                    continue
+                 # Reinitialize the text variable
+                 text = ""
 
 if __name__ == "__main__":
     app.run()
