e3b378244a2561c82109d0beba6c2e008a10dc18
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2024-01-13 16:19:25 -0500

    pushing mvp nlp code

--- nlp.py
+++ nlp.py
@@ -29,6 +29,7 @@ def text_cleanup(text):
   
   # Join the lemmatized tokens into a sentence
   lemmatized_text = ' '.join(lemmatized_tokens)
+  #TODO remove more symbols
   cut_punctuation = punctuation.replace('.','')
 
   # TODO keep periods eventually for splitting on
@@ -58,11 +59,12 @@ def get_hotwords(text, query):
             result.append(token.text)
     return result
 
-def get_hot_words_keybert(text, query):
+def get_hot_words_keybert(text, query, model):
    kw_model = KeyBERT()
    # get all hot word data
    hot_words = kw_model.extract_keywords(text, stop_words='english', top_n=-1)
    hot_words = list(filter(lambda hot_word: hot_word[0] in query.split(' '), hot_words))
+   hot_words = list(filter(lambda hot_word: hot_word[0] in model.wv.key_to_index, hot_words))
    # no keywords found in query
    if len(hot_words) == 0:
       return hot_words
@@ -90,7 +92,7 @@ def selection(text, hotwords, orig_text):
     # getting the average between all of hotwords, determining relevance of sentence
     avg_score = sum(best_hotwords) / len(best_hotwords)
     # arbitrary cutoff for adding the sentence
-    if avg_score > .85:
+    if avg_score > .9:
       res_sentences.append(orig_sentences[i])
   return res_sentences
 
@@ -102,33 +104,38 @@ def selection_word2vec(text, hot_words, model, orig_text):
   pos_tag = ['PROPN', 'ADJ', 'NOUN'] 
   for i, sentence in enumerate(sentences):
     best_hotwords = [0]*len(hot_words)
-    enum_sentence = sentence.split(" ")
-    words = nlp(sentence) 
-    for word_info in words:
-      word = word_info.text
-      pos = word_info.pos_
-      if pos in pos_tag:
-        for j, hot_word in enumerate(hot_words):
+    count_hotwords = [0]*len(hot_words)
+    words = nlp(sentence)
+    for j, hot_word in enumerate(hot_words): 
+      for word_info in words:
+        word = word_info.text
+        pos = word_info.pos_
+        if pos in pos_tag:
             try:
               cur_sim = model.wv.similarity(word, hot_word[0]) 
-              adj_sim = cur_sim ** 4
-              # print(word, hot_word[0], cur_sim, adj_sim)
+              if cur_sim == 1: # best possible word found
+                best_hotwords[j] = 1
+                count_hotwords[j] = word # make division be equal to one
+                break
+              adj_sim = cur_sim
               adj_sim = adj_sim * hot_word[1] # multiplying by relevance
               adj_sim += 1 - hot_word[1] # make less relevant words matter less
+              # idea is to only do this for cur sim = 1, otherwise do average
               if adj_sim > best_hotwords[j]:
                 # print(word, hot_word[0], cur_sim)
                 best_hotwords[j] = adj_sim
-              if cur_sim == 1: # best possible word found
-                break
+                count_hotwords[j] = word
             except Exception as error: # if key not present in sim
-              break
+              continue
+    # best_hotwords = [0 if c == 0 else h / c for h,c in zip(best_hotwords, count_hotwords)]
     # getting the average between all of hotwords, determining relevance of sentence
     avg_score = sum(best_hotwords) / len(best_hotwords)
-    print(best_hotwords, avg_score)
     # arbitrary cutoff for adding the sentence
-    if avg_score > .85:
+    if avg_score > 1 - .1 * (3 if len(best_hotwords) > 3 else len(best_hotwords)):
+      # print(best_hotwords, avg_score)
+      # print(count_hotwords, hot_words)
       res_sentences.append(orig_sentences[i])
-  # return res_sentences
+  return res_sentences
 
 def tokenizer(text):
   res_tokens = []
@@ -141,73 +148,79 @@ def tokenizer(text):
       res_tokens.append(temp)
   return res_tokens
 # Define a sample text
-text = """
-When it comes to evaluating the performance of keyword extractors, you can use some of the standard metrics in machine learning: accuracy, precision, recall, and F1 score. However, these metrics don’t reflect partial matches. they only consider the perfect match between an extracted segment and the correct prediction for that tag.
-Fortunately, there are some other metrics capable of capturing partial matches. An example of this is ROUGE.
-"""
-sample = open("./alice.txt")
-text = sample.read()
-text = text.replace("\n", " ")
-# print("Original Text:", text)
-new_text = text_cleanup(text)
+# text = """
+# When it comes to evaluating the performance of keyword extractors, you can use some of the standard metrics in machine learning: accuracy, precision, recall, and F1 score. However, these metrics don’t reflect partial matches. they only consider the perfect match between an extracted segment and the correct prediction for that tag.
+# Fortunately, there are some other metrics capable of capturing partial matches. An example of this is ROUGE.
+# """
+# sample = open("./alice.txt")
+# text = sample.read()
+# text = text.replace("\n", " ")
+# # print("Original Text:", text)
+# new_text = text_cleanup(text)
 
 # print("Removed text: ", new_text)
 
-query = """
-i want a story about how alice performed best under pressure.
-"""
-query = text_cleanup(query)
-# output = set(get_hotwords(new_text, query))
-# # TODO get least common instead of most?
-# most_common_list = Counter(output).most_common(10)
-# for item in most_common_list:
-#   print(item[0])
-# print(selection(new_text, most_common_list, text))
+# query = """
+# i want a story about how alice performed best under pressure.
+# """
+# query = """
+# Who is Alice's biggest friend?
+# """
+# query = """
+# are pigeons sad?
+# """
+# query = """
+# i want a story about how alice performed best under pressure with the Queen.
+# """
+# query = text_cleanup(query)
 
-
-# Create CBOW model
-model1 = gensim.models.Word2Vec(tokenizer(new_text), min_count=1,
-                                vector_size=100, window=5)
-# Print results
-print("Cosine similarity between 'alice' " +
-      "and 'wonderland' - CBOW : ",
-      model1.wv.similarity('alice', 'wonderland'))
-# print("Cosine similarity between 'perfect' " +
-#       "and 'match' - CBOW : ",
-#       model1.wv.similarity('perfect', 'match'))
+# # Create CBOW model
+# model1 = gensim.models.Word2Vec(tokenizer(new_text), min_count=3,
+#                                 vector_size=100, window=5, epochs=50)
+# # Print results
 # print("Cosine similarity between 'alice' " +
-#       "and 'machines' - CBOW : ",
-#       model1.wv.similarity('keyword', 'match'))
- 
-# Create Skip Gram model
-model2 = gensim.models.Word2Vec(tokenizer(new_text), min_count=1, vector_size=100,
-                                window=5, sg=1)
- 
-# Print results
-print("Cosine similarity between 'alice' " +
-      "and 'wonderland' - Skip Gram : ",
-      model2.wv.similarity('alice', 'wonderland'))
-# print("Cosine similarity between 'perfect' " +
-#       "and 'match' - CBOW : ",
-#       model2.wv.similarity('perfect', 'match'))
+#       "and 'wonderland' - CBOW : ",
+#       model1.wv.similarity('alice', 'wonderland'))
+# model2 = gensim.models.Word2Vec(tokenizer(new_text), min_count=3, vector_size=100,
+#                                 window=3, sg=1, epochs=500)
+# # model2 = gensim.models.Word2Vec(list(map(lambda x: x.split(" "), new_text.split("."))), min_count=1, vector_size=100,
+# #                                 window=5, sg=1)
+# # Print results
 # print("Cosine similarity between 'alice' " +
-#       "and 'machines' - Skip Gram : ",
-#       model2.wv.similarity('keyword', 'match'))
+#       "and 'wonderland' - Skip Gram : ",
+#       model2.wv.similarity('alice', 'wonderland'))
+# # print("Cosine similarity between 'perfect' " +
+# #       "and 'match' - CBOW : ",
+# #       model2.wv.similarity('perfect', 'match'))
+# # print("Cosine similarity between 'alice' " +
+# #       "and 'machines' - Skip Gram : ",
+# #       model2.wv.similarity('keyword', 'match'))
 
-new_hotwords = get_hot_words_keybert(new_text, query)
-print(new_hotwords)
-print(selection_word2vec(new_text, new_hotwords, model2, text))
-# potentially unnecessary stuff
-# max_summary_len = 15
-# #prepare a tokenizer for reviews on training data
-# y_tokenizer = Tokenizer(num_words=tot_cnt-cnt) 
-# y_tokenizer.fit_on_texts(list(y_tr))
-# y_val = list(map(lambda x: str(x), y_val))
+# new_hotwords = get_hot_words_keybert(new_text, query, model2)
+# print(new_hotwords)
+# res = selection_word2vec(new_text, new_hotwords, model2, text)
+# for p in res:
+#    print([p])
 
-# #convert text sequences into integer sequences (i.e one hot encode the text in Y)
-# y_tr_seq    =   y_tokenizer.texts_to_sequences(y_tr) 
-# y_val_seq   =   y_tokenizer.texts_to_sequences(y_val) 
+# print(new_hotwords, len(res))
 
-# #padding zero upto maximum length
-# y_tr    =   pad_sequences(y_tr_seq, maxlen=max_summary_len, padding='post')
-# y_val   =   pad_sequences(y_val_seq, maxlen=max_summary_len, padding='post')
+def ctrl_f(input, query):
+  cleaned_input = text_cleanup(input)
+  cleaned_query = text_cleanup(query)
+  model = gensim.models.Word2Vec(tokenizer(cleaned_input), min_count=3, vector_size=100,
+                              window=3, sg=1, epochs=500)
+  query_hotwords = get_hot_words_keybert(cleaned_input, cleaned_query, model)
+  print('hotwords:', query_hotwords)
+  res_sentences = selection_word2vec(cleaned_input, query_hotwords, model, input)
+  print('sentences found:', len(res_sentences))
+  return res_sentences
+
+# sample = open("./alice.txt")
+# text = sample.read()
+# text = text.replace("\n", " ")
+# query = """
+# i want a story about how alice performed best under pressure.
+# """
+# res = ctrl_f(text, query)
+# for p in res:
+#    print([p])
